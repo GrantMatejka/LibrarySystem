b3674b25220504c49eb8c68c3ac328f9c777cdd2
Grant Matejka <grantmatejka1@gmail.com>
2020-03-16 19:38:17 -0700

    Dao Implementations

---
+++ src/main/java/edu/calpoly/csc365/dao/BookDao.java
@@ -0,0 +1,7 @@
+package edu.calpoly.csc365.dao;
+
+import edu.calpoly.csc365.entity.Book;
+
+public interface BookDao extends Dao<Book> {
+    //Method signatures for gathering data on books goes here
+}
---
+++ src/main/java/edu/calpoly/csc365/dao/TransactionDao.java
@@ -0,0 +1,8 @@
+package edu.calpoly.csc365.dao;
+
+import edu.calpoly.csc365.entity.Transaction;
+
+public interface TransactionDao extends Dao<Transaction> {
+    //Method signatures for gathering data on transactions goes here
+
+}
