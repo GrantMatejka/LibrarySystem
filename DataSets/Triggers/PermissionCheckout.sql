aff03e653783ec783bda026270330f33887a90af
Grant Matejka <grantmatejka1@gmail.com>
2020-03-19 10:15:15 -0700

    Fixes for any student type

--- DataSets/Triggers/PermissionCheckout.sql
+++ DataSets/Triggers/PermissionCheckout.sql
@@ -1,13 +1,16 @@
+use group09;
+drop trigger  checkout_period;
+
 DELIMITER $
 CREATE TRIGGER `checkout_period` BEFORE INSERT ON `Transactions`
 FOR EACH ROW
 BEGIN
-	SET NEW.expectedCheckInDate = NEW.checkOutDate + 
+	SET NEW.expectedCheckInDate = DATE_ADD(NEW.checkOutDate, INTERVAL
 		(
         SELECT p.checkoutPeriod 
         FROM Permissions p
         JOIN Users u ON u.permissionLevel = p.permissionLevel
         WHERE u.id = NEW.userId
-		);
+		) DAY);
 
 END$ DELIMITER ;
