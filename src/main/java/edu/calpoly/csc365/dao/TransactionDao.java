43edde735c316dba6a6503da801b6b9351f50bb7
Rohan Ramani <rramani1230@gmail.com>
2020-03-19 02:18:16 -0700

    Trans

--- src/main/java/edu/calpoly/csc365/dao/TransactionDao.java
+++ src/main/java/edu/calpoly/csc365/dao/TransactionDao.java
@@ -3,6 +3,5 @@ package edu.calpoly.csc365.dao;
 import edu.calpoly.csc365.entity.Transaction;
 
 public interface TransactionDao extends Dao<Transaction> {
-    //Method signatures for gathering data on transactions goes here
-
+    public Set<Transaction> getAll();
 }
